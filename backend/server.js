62a99e4f9933059daf82da964c89768eb4dd1e4a
JamesT-1 <taxmatzidis1@gmail.com>
2026-03-18 15:52:06 +0200

    login problem fixed

--- backend/server.js
+++ backend/server.js
@@ -18,14 +18,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV !== "production") {
-  app.use(
-    cors({
-      origin: "http://localhost:5173",
-      credentials: true,
-    })
-  );
-}
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
@@ -41,9 +39,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.use((req, res, next) => {
     if (!req.path.startsWith("/api")) {
-      res.sendFile(
-        path.resolve(__dirname, "../frontend", "dist", "index.html")
-      );
+      res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     } else {
       next();
     }
